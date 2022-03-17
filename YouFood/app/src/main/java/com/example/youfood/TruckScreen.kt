b3b8815669a9ff75b3979697a4ee6ce1ced75696
Alexander <covaalexander06@gmail.com>
2022-03-16 21:32:15 -0400

    added some of the location code, continued some small fixes

--- YouFood/app/src/main/java/com/example/youfood/TruckScreen.kt
+++ YouFood/app/src/main/java/com/example/youfood/TruckScreen.kt
@@ -5,8 +5,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import com.github.kittinunf.fuel.Fuel
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class TruckScreen : AppCompatActivity() {
+class TruckScreen : AppCompatActivity(), OnMapReadyCallback {
+
+    private var lon : Double = 0.0
+    private var lat : Double = 0.0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_truck_screen)
@@ -24,15 +33,31 @@ class TruckScreen : AppCompatActivity() {
 
                     var infoArray = response.toTypedArray()
 
+                    load_screen(infoArray)
+
                 }
 
             }
+
+        val map = getSupportFragmentManager().findFragmentById(R.id.map) as SupportMapFragment
+        map.getMapAsync(this)
     }
 
     private fun load_screen(info : Array<String>) {
         val name = info[0]
         val email = info[1]
-        val isOpen = info[4]
         val food = info[3]
+        val isOpen = info[4]
+        lon = info[5].toDouble()
+        lat = info[6].toDouble()
+
+
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        googleMap.addMarker(
+            MarkerOptions()
+                .position(LatLng(lat, lon))
+        )
     }
 }
