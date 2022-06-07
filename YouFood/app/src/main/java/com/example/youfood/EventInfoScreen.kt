6f012f950bdd8f6391ed173a95869a49e739ecfa
Alexander <covaalexander06@gmail.com>
2022-06-07 09:54:53 -0400

    Refactored Event interested and uninterested code
    still needs to be tested, but also changed on server side

--- YouFood/app/src/main/java/com/example/youfood/EventInfoScreen.kt
+++ YouFood/app/src/main/java/com/example/youfood/EventInfoScreen.kt
@@ -1,18 +1,21 @@
 package com.example.youfood
 
 import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.*
-import androidx.core.database.getIntOrNull
 import com.github.kittinunf.fuel.Fuel
 import com.github.kittinunf.fuel.coroutines.awaitStringResponseResult
 import kotlinx.coroutines.runBlocking
 import java.io.File
 
 class EventInfoScreen : AppCompatActivity() {
+
+    var interested : Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_event_info_screen)
@@ -26,24 +29,51 @@ class EventInfoScreen : AppCompatActivity() {
         val accountFile = File(filesDir, "records.txt").readLines()
         val email = accountFile[0]
 
-        val file = File(filesDir, "events.txt")
-        if (file.exists()) {
-            val eventList = file.readLines().toMutableList()
-            var eventIndex = eventList.indexOf(name)
+        load_screen(name!!, email)
 
-            if (eventIndex != -1) {
-                goingButton.text = getString(R.string.uninterested)
+
+
+
+        goingButton.setOnClickListener {
+            if (interested) {
+                runBlocking {
+                    val (_,_, result) = Fuel.post("http://foodtruckfinder.com/unattending-event", listOf("name" to name, "account" to email)).awaitStringResponseResult()
+
+                    result.fold(
+                        { data ->
+                            load_screen(name, email)
+                        },
+                        { error -> Log.e("http", "$error")}
+                    )
+                }
             } else {
-                goingButton.text = getString(R.string.interested)
-            }
+                runBlocking {
+                    val (_,_, result) = Fuel.post("http://foodtruckfindermi.com/attending-event", listOf("name" to name, "account" to email)).awaitStringResponseResult()
 
+                    result.fold(
+                        { data ->
+                            load_screen(name, email)
+                        },
+                        { error -> Log.e("http", "$error")}
+                    )
+                }
+            }
         }
 
 
 
+    }
+
+    fun load_screen(name : String, email : String) {
+        val goingLabel = findViewById<TextView>(R.id.goingAmount)
+        val nameLabel = findViewById<TextView>(R.id.eventName)
+        val goingButton = findViewById<Button>(R.id.goingButton)
 
         runBlocking {
-            val (_, _, result) = Fuel.get("http://foodtruckfindermi.com/get-event-info", listOf("name" to name))
+            val (_, _, result) = Fuel.get(
+                "http://foodtruckfindermi.com/get-event-info",
+                listOf("name" to name, "account" to email)
+            )
                 .awaitStringResponseResult()
 
             result.fold(
@@ -56,72 +86,27 @@ class EventInfoScreen : AppCompatActivity() {
                     val date = eventInfoList[2]
                     val city = eventInfoList[3]
                     val going = eventInfoList[5]
+                    val isGoing = eventInfoList[6]
 
 
                     nameLabel.text = name
                     goingLabel.text = going
 
+                    if (isGoing == "true") {
+                        goingButton.text = getString(R.string.uninterested)
+                        interested = true
+                    } else {
+                        goingButton.text = getString(R.string.interested)
+                        interested = false
+                    }
 
 
                 },
-                { error -> Log.e("http", "$error")}
+                { error -> Log.e("http", "$error") }
             )
         }
-
-
-
-        goingButton.setOnClickListener {
-            val file = File(filesDir,"events.txt")
-
-            if (file.exists()) {
-                val eventList = file.readLines().toMutableList()
-                var eventIndex = eventList.indexOf(name)
-
-                if (eventIndex != -1) {
-                    eventIndex += 1
-
-                    if (eventList[eventIndex] == "0") {
-                        runBlocking {
-                            val (_, _, result) = Fuel.post("http://foodtruckfindermi.com/attending-event", listOf("name" to name, "account" to email))
-                                .awaitStringResponseResult()
-
-                            eventList[eventIndex] = "1"
-                            for(i in eventList) {
-                                file.writeText(i + "\n")
-                            }
-                        }
-                    } else {
-                        runBlocking {
-                            val (_, _, result) = Fuel.post("http://foodtruckfindermi.com/unattending-event", listOf("name" to name))
-                                .awaitStringResponseResult()
-
-                            eventList.removeAt(eventIndex)
-                            eventList.removeAt(eventIndex - 1)
-                            for (i in eventList){
-                                file.writeText(i + "\n")
-                            }
-                        }
-                    }
-                } else {
-                    file.appendText(name + "\n" + "1")
-                }
-
-            } else {
-                runBlocking {
-                    file.createNewFile()
-
-                    val (_, _, result) = Fuel.post(
-                        "http://foodtruckfindermi.com/attending-event",
-                        listOf("name" to name, "account" to email)
-                    ).awaitStringResponseResult()
-
-                    val record = name + "\n" + 1
-                    file.appendText(record)
-                }
-            }
-        }
-
-
-
     }
 }
+
+
+
--- YouFood/app/src/main/java/com/example/youfood/MainActivity.kt
+++ YouFood/app/src/main/java/com/example/youfood/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.youfood
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
@@ -8,7 +9,6 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.database.getStringOrNull
 import com.github.kittinunf.fuel.Fuel
 import com.github.kittinunf.fuel.httpGet
 import java.io.File
